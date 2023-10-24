f5bd3c9541d02eec6061df88e2f4dd30d188887f
andrey <andrey@yandex.ru>
2023-10-24 13:27:08 +0300

    Добавьте файлы проекта.

---
+++ задание 1/задание 1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    int numVisitors;
+   cout << "Введите количество посетителей спортзала за день: ";
+    cin >> numVisitors;
+
+    vector<int> ages(numVisitors);
+    int age;
+
+    for (int i = 0; i < numVisitors; ++i) {
+       cout << "Введите возраст посетителя " << (i + 1) << ": ";
+        cin >> age;
+        ages[i] = age;
+    }
+
+    int minAge = ages[0];
+    int maxAge = ages[0];
+
+    int totalAge = ages[0];
+
+    for (int i = 1; i < numVisitors; ++i) {
+        if (ages[i] < minAge) {
+            minAge = ages[i];
+        }
+        if (ages[i] > maxAge) {
+            maxAge = ages[i];
+        }
+        totalAge += ages[i];
+    }
+
+    double averageAge = static_cast<double>(totalAge) / numVisitors;
+
+    cout << "Самый молодой посетитель: " << minAge << " лет\n";
+    cout << "Самый старший посетитель: " << maxAge << " лет\n";
+    cout << "Средний возраст посетителей: " << averageAge << " лет\n";
+
+    return 0;
+}
+
